341b8d624389c0f62cfbe76fbfb0e4309cd5c23c
Olivier Penhoat <openhoat@gmail.com>
2024-02-19 15:07:53 +0100

    [FEAT] Add auth context provider to nav bar

--- src/frontend/main/base.tsx
+++ src/frontend/main/base.tsx
@@ -4,15 +4,18 @@ import { StrictMode, Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
+import { AuthProvider } from './providers/AuthProvider'
 
 export const createApp = (): ReactNode => {
   return (
     <StrictMode>
       <BrowserRouter>
         <Suspense fallback={<h3>Loading…</h3>}>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
+          <AuthProvider>
+            <ThemeProvider>
+              <App />
+            </ThemeProvider>
+          </AuthProvider>
         </Suspense>
       </BrowserRouter>
     </StrictMode>
