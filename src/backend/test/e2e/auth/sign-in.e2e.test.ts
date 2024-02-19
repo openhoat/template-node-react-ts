f06a6090de25ae0ed49c0b1eb411df1be3512af3
Olivier Penhoat <openhoat@gmail.com>
2024-02-19 15:06:20 +0100

    [TEST] Add /sign-in e2e test

---
+++ src/backend/test/e2e/auth/sign-in.e2e.test.ts
@@ -0,0 +1,29 @@
+import { regex } from 'pactum-matchers'
+
+import { getTestCase, registerHooks } from '../util/server-test-hooks'
+
+describe('backend tests', () => {
+  describe('backend server e2e tests', () => {
+    registerHooks()
+    describe('POST /auth/sign-in', () => {
+      test('should respond a token with secured cookie given credentials', async () => {
+        // Given
+        const username = 'johndoe'
+        const password = 'MyBigSecret'
+        const testCase = getTestCase()
+        await testCase
+          .step('Create a token')
+          .spec()
+          // When
+          .post('/auth/sign-in')
+          .withBody({ password, username })
+          // Then
+          .expectStatus(200)
+          .expectJsonMatch({
+            token: regex(/^([\w=]+).([\w=]+).([\w+/=-]*)$/),
+          })
+          .toss()
+      })
+    })
+  })
+})
